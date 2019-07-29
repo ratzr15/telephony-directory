f6f42af1367752ef9b2ba4a59457304683c816ea
Rathish Kannan <rathish.k@sunandsandsports.com>
2019-07-29 15:15:34 +0400

    Extend delegates for action

--- Telephony-Directory/Detail/DetailViewController.swift
+++ Telephony-Directory/Detail/DetailViewController.swift
@@ -11,6 +11,9 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
+import MessageUI
+import SafariServices
 
 final class DetailViewController: UIViewController, ListResourcesViewController, CanModifyTableView {
     typealias ViewModel = DetailViewModel
@@ -37,16 +40,16 @@ final class DetailViewController: UIViewController, ListResourcesViewController,
         return ErrorView()
     }()
     
-    lazy var sortButton: UIBarButtonItem = {
-        return UIBarButtonItem.init(barButtonSystemItem: .add, target: nil, action: nil)
+    lazy var modifyButton: UIBarButtonItem = {
+        return UIBarButtonItem(title: "Edit", style: .plain, target: nil, action: nil)
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Contact"
         setupBindings()
         setupViewHierarchy()
         setupConstraints()
+        setupDetailBindings()
         viewModel.requestResource(id: String(viewModel.resourceEntities[0].id ?? 0))
         navigationItem.largeTitleDisplayMode = .never
     }
@@ -63,8 +66,7 @@ final class DetailViewController: UIViewController, ListResourcesViewController,
         view.addSubview(errorView)
         view.addSubview(loadingView)
         view.addSubview(tableView)
-        
-        tableView.register(UINib(nibName: ListTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: ListTableViewCell.identifier)
+        tableView.register(UINib(nibName: DetailTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: DetailTableViewCell.identifier)
     }
 
     private func setupConstraints() {
@@ -87,5 +89,75 @@ final class DetailViewController: UIViewController, ListResourcesViewController,
             make.bottom.equalToSuperview().inset(self.view.safeAreaInsets)
         }
     }
+    
+    private func setupDetailBindings() {
+        
+        viewModel.selectedEmail
+            .flatMap({ return $0 == nil ? Observable.empty() : Observable.just($0!) })
+            .subscribe(onNext: { [weak self] email in self?.composeEmail(email: email) })
+            .disposed(by: bag)
+        
+        viewModel.selectedCallPhoneNumber
+            .flatMap({ return $0 == nil ? Observable.empty() : Observable.just($0!) })
+            .subscribe(onNext: { [weak self] number in self?.callNumber(number: number) })
+            .disposed(by: bag)
+        
+        viewModel.selectedSmsPhoneNumber
+            .flatMap({ return $0 == nil ? Observable.empty() : Observable.just($0!) })
+            .subscribe(onNext: { [weak self] number in self?.smsNumber(number: number) })
+            .disposed(by: bag)
+    }
+}
 
+
+extension DetailViewController {
+    fileprivate func composeEmail(email: String) {
+        guard MFMailComposeViewController.canSendMail() else {
+            showFeatureNotSupportedAlert()
+            return
+        }
+        let mail = MFMailComposeViewController()
+        mail.mailComposeDelegate = self
+        mail.setToRecipients([email])
+        mail.setMessageBody("", isHTML: true)
+        present(mail, animated: true)
+    }
+    
+    fileprivate func callNumber(number: String) {
+        guard let number = URL(string: "tel://" + number), UIApplication.shared.canOpenURL(number) else {
+            showFeatureNotSupportedAlert()
+            return
+        }
+        UIApplication.shared.open(number)
+    }
+
+    fileprivate func smsNumber(number: String) {
+        guard MFMessageComposeViewController.canSendText() else {
+            showFeatureNotSupportedAlert()
+            return
+        }
+        let sms = MFMessageComposeViewController()
+        sms.messageComposeDelegate = self
+        sms.recipients = [number]
+        present(sms, animated: true)
+    }
+
+    private func showFeatureNotSupportedAlert(message: String = "Your device does not support this feature") {
+        let alert = UIAlertController(title: "Failure", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Discard", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
+}
+
+extension DetailViewController : MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true)
+    }
 }
