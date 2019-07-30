8b89f8152cdc3471837023acf061e311e7bde673
Rathish Kannan <rathish.k@sunandsandsports.com>
2019-07-30 17:21:46 +0400

    Extend init for easier testing VM

--- Telephony-Directory/API/Model/BusinessEntities/Contact.swift
+++ Telephony-Directory/API/Model/BusinessEntities/Contact.swift
@@ -1,26 +1,16 @@
-/* 
-Copyright (c) 2019 Swift Models Generated from JSON powered by http://www.json4swift.com
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar
-
-*/
-
 import Foundation
+
+typealias JSON = Dictionary<String, Any>
+
 public struct Contact : Codable, Resource {
     public var id : Int?
-	public let first_name : String?
-	let last_name : String?
-	let profile_pic : String?
-	let favorite : Bool?
-	let url : String?
-    let phone_number : String?
-    let email : String?
+	public var first_name : String?
+	var last_name : String?
+	var profile_pic : String?
+	var favorite : Bool?
+	var url : String?
+    var phone_number : String?
+    var email : String?
 
 	enum CodingKeys: String, CodingKey {
 
@@ -34,7 +24,7 @@ public struct Contact : Codable, Resource {
         case email = "email"
 	}
 
-	public init(from decoder: Decoder) throws {
+    public  init(from decoder: Decoder) throws {
 		let values = try decoder.container(keyedBy: CodingKeys.self)
 		id = try values.decodeIfPresent(Int.self, forKey: .id)
 		first_name = try values.decodeIfPresent(String.self, forKey: .first_name)
@@ -47,4 +37,17 @@ public struct Contact : Codable, Resource {
 
 	}
 
+    init(json: JSON) {
+        guard let id = json["id"] as? Int,
+            let firstname = json["firstname"] as? String,
+            let lastname = json["lastname"] as? String,
+            let phonenumber = json["phonenumber"] as? String else {
+                return
+        }
+        self.id = id
+        self.first_name = firstname
+        self.last_name = lastname
+        self.phone_number = phonenumber
+    }
+    
 }
